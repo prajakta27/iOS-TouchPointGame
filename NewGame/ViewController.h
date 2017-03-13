d98c59af11ccf82f3b3e645e132e646f2646f3ec
prajakta sonawane <prajakta.sonawane@porteamedical.com>
2017-03-13 17:46:37 +0530

    Initial Commit

---
+++ NewGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewGame
+//
+//  Created by Prajakta Vishwas Sonawane on 3/13/17.
+//  Copyright © 2017 Prajakta Vishwas Sonawane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewGame/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NewGame
+//
+//  Created by Prajakta Vishwas Sonawane on 3/13/17.
+//  Copyright © 2017 Prajakta Vishwas Sonawane. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
